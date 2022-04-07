62e8511e741eaed6a522a3e08a0e087e13883bf4
Cesar <ipog71@gmail.com>
2022-04-07 11:32:22 -0500

    Ya reproduce musica

--- prueba musica 3.cpp
+++ prueba musica 3.cpp
@@ -1,33 +1,15 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <Windows.h>
+#include <string>
 
-typedef BOOL(PlaySoundA_ptr)(char, HMODULE, DWORD);
-PlaySoundA_ptr fn_PlaySound = 0;
+using namespace std;
 
 int main()
 {
-    HMODULE Lib = LoadLibrary("winmm.dll");
-    if (Lib)
-    {
-        fn_PlaySound = (PlaySoundA_ptr)GetProcAddress(Lib, "PlaySoundA");
-        if (fn_PlaySound)
-        {
-            fn_PlaySound("Music.wav", NULL, SND_FILENAME | SND_ASYNC);
-        }
-        else
-        {
-            MessageBox(0, "Error", "No se encontro la función PlaySoundA", 0);
-        }
-        getch();
-        getch();
-        FreeLibrary(Lib);
-    }
-    else
-    {
-        MessageBox(0, "Error", "No se pudo cargar winmm.dll", 0);
-    }
-    return 0;
+    string cancion = "Music.wav";
+    cout<<PlaySound((LPCSTR)cancion.c_str(),NULL, SND_FILENAME|SND_ASYNC);
     system("PAUSE");
 }
