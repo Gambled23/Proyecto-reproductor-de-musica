49501d9b6e58b889101b3d0f60659403471093b1
Cesar <ipog71@gmail.com>
2022-04-07 11:36:43 -0500

    Prueba para concatenar strings(termina sexual)

---
+++ concatenar strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+int main()
+{
+    string str1 = "Hola amigos de ";
+    string str2 = "pablo";
+    string str3 = str1 + str2;
+    cout<<str3;
+    system("PAUSE");
+    return 0;
+}
